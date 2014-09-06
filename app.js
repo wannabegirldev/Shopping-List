02f07e5e8ff379a42573a5f5ec12d971b73d349d
wannabegirldev <ottfamily@Leslie-Picos-iMac.local>
2014-09-05 18:27:17 -0700

    List adds and deletes

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-$(document).ready(function(){
-    $('#toadd').click(function(e) {
+$(document).ready(function () {
+    function addItemToList(e) {
         var addItem = $('#item').val();
-    $('ul').append('<li>' + addItem);
- });
-});
+        var $li = $('<li>' + '<button class="uibutton"></button>' + addItem + '</li>').appendTo('.shopping');
 
+        /*target only newly added button*/
+        $li.find('.uibutton').button({
+            icons: {
+                primary: "ui-icon-heart"
+            },
+            text: false
+        });
+        $('#item').val('');
+    }
+
+    /*use event delegation instead of adding the handler in another event handler*/ 
+    $('ul.shopping').on('click', '.uibutton', function () {
+        $(this).closest('li').remove();
+    });
+
+    /*adds list item to list when cart icon is clicked, clears #item input field*/
+    $('#toadd').click(function (e) {
+        addItemToList(e);
+    });
+
+    /*adds list item to list when enter key is hit, clears #item input field*/
+    $('#item').keydown(function (e) {
+        if (e.which == 13) {
+            addItemToList(e);
+        }
+    });
+});
     
-        
